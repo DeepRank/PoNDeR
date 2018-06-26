fcbd4849bd248cfb0559b0de9355cf5db5ee1485
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-06-26 16:21:44 +0200

    Removed legacy code

--- utils.py
+++ utils.py
@@ -1,14 +1,6 @@
-decoy_fn = '1AK4_100w.pdb'
-print(decoy_fn[:-9] + '.pdb')
-
 # Get current learning rate from optimizer
 def get_lr(optimizer):
     lr=[]
     for param_group in optimizer.param_groups:
        lr +=[param_group['lr']]
     return lr
-
-# Get filename for native conformation from filename for simulated conformation
-def get_ref(decoy_fn):
-    ref_fn = decoy_fn.rsplit( "_", 1 )[ 0 ] + '.pdb'
-    return ref_fn 
