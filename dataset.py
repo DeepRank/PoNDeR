d11cf6ed5193c99a1fd20bdf42c076af0b0a25e1
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-06-26 16:34:18 +0200

    Rudimentary dataset for one subset

--- dataset.py
+++ dataset.py
@@ -1,12 +1,16 @@
 import torch.utils.data as data
+import pickle
 
 class PDB(data.Dataset):
     def __init__(self, root_dir, train = True, transform = None, num_points=2500):
         self.root_dir = root_dir
         self.transform = transform
 
+        pc_file = '/home/lukas/test.pickle'
+        with open(pc_file, "rb") as f:
+            self.irmsds, self.pcs = pickle.load(f)
     def __len__(self):
-        return TODO
+        return len(self.irmsds)
 
     def __getitem__(self, idx):
-        return TODO
+        return self.pcs[idx], self.pcs[idx]
