0619f8147ac80c43ebe5f4911d04dee6ea6115c0
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-06-27 15:39:50 +0200

    Dataset pickle loading + point sampling

--- dataset.py
+++ dataset.py
@@ -1,16 +1,33 @@
+import torch
 import torch.utils.data as data
 import pickle
+import os
+import random
+import numpy as np
 
-class PDB(data.Dataset):
-    def __init__(self, root_dir, train = True, transform = None, num_points=2500):
+class PDBset(data.Dataset):
+    def __init__(self, root_dir, train = True, num_points=250):
+        self.file_list = os.listdir(root_dir)
         self.root_dir = root_dir
-        self.transform = transform
+        self.num_points = num_points
 
-        pc_file = '/home/lukas/test.pickle'
-        with open(pc_file, "rb") as f:
-            self.irmsds, self.pcs = pickle.load(f)
     def __len__(self):
-        return len(self.irmsds)
+        return len(self.file_list)
 
     def __getitem__(self, idx):
-        return self.pcs[idx], self.pcs[idx]
+        pcs = []
+        irmsds = []
+
+        for i, val in enumerate(idx):
+            with open(self.root_dir+self.file_list[val], "rb") as f:
+                irmsd, pc = pickle.load(f)
+                
+                if len(pc) < self.num_points:
+                    point_ids = random.sample(range(self.num_points), len(pc)) + random.sample(range(self.num_points), self.num_points-len(pc))
+                else:
+                    point_ids = random.sample(range(self.num_points), self.num_points)
+
+                irmsds.append(irmsd)
+                pcs.append(np.take(pc,point_ids,axis=0))
+                
+        return torch.from_numpy(np.stack(pcs)), irmsds
