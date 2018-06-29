dd165cb650075d5e4bbd4f19e581f65c604deaa8
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-06-29 17:06:54 +0200

    Update hyperparameters

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python train.py --data_path $1 --out_folder $2  --num_epoch 3 --cosine_decay | tee log.txt
+python train.py --data_path $1 --out_folder $2  --num_epoch 3 --cosine_decay --batch_size 100 --num_workers 4 --num_points 500| tee log.txt
