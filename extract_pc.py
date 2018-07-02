95da6a7e43657f9d43fc5e446c38152b44005a3f
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-02 16:32:24 +0200

    Switched to HDF5 format

--- extract_pc.py
+++ extract_pc.py
@@ -2,87 +2,85 @@ import pkg_resources
 import argparse
 import os
 import numpy as np
-import pickle
 import torch
 import sys
+import h5py
+import random
 
 from deeprank.features import AtomicFeature
 from deeprank.tools import StructureSimilarity
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--train', dest='train', default=True,action='store_false', help='Train data if True, test data if False')
 parser.add_argument('--root_dir', type=str, help='Absolute path to data')
 
 arg = parser.parse_args(['--root_dir', '/home/lukas/DR_DATA/'])
 
-if arg.train:
-    subfolder = 'train/'
-else:
-    subfolder = 'test/'
-
-# Force field provided with deeprank
+""" # Force field provided with deeprank
 FF = pkg_resources.resource_filename('deeprank.features', '') + '/forcefield/'
 param_charge = FF + 'protein-allhdg5-4_new.top'
 param_vdw = FF + 'protein-allhdg5-4_new.param'
 patch_file = FF + 'patch.top'
 
-label_encoder = LabelEncoder()
-onehot_encoder = OneHotEncoder(sparse=False)
+hf = h5py.File('pointclouds.h5', 'w')
 
+g_test = hf.create_group('test')
+g_train = hf.create_group('train')
+g_holdout = hf.create_group('holdout')
 
-def convertFolder(native_name):
+def getSubgroup(native_name):
+    rand = random.random()
+    if rand < 0.8:
+        subgroup = g_train.create_group(native_name[:4])
+    elif rand < 0.9:
+        subgroup = g_test.create_group(native_name[:4])
+    else:
+        subgroup = g_holdout.create_group(native_name[:4])
+    return subgroup
+
+for native_name in sorted(os.listdir(arg.root_dir+'natives/')):
     if native_name.endswith(".pdb"):
         decoy_dir = arg.root_dir+'decoys/'+native_name[:4]
         if os.path.isdir(decoy_dir):
-            for decoy_name in os.listdir(decoy_dir):
-                pc_file = arg.root_dir + 'pointclouds/' + \
-                    subfolder + decoy_name[:-4] + '.pickle'
-                if os.path.isfile(pc_file):
-                    print(decoy_name[:-4], 'skipped')
-                else:
-                    try:
-                        # Declare the feature calculator instance
-                        atFeat = AtomicFeature(decoy_dir+'/'+decoy_name, param_charge=param_charge, param_vdw=param_vdw, patch_file=patch_file)
+            subgroup = getSubgroup(native_name)
+            print('Putting', native_name[:4], 'in', subgroup.name)
+            for decoy_name in sorted(os.listdir(decoy_dir)):
+                # Declare the feature calculator instance
+                atFeat = AtomicFeature(decoy_dir+'/'+decoy_name, param_charge=param_charge, param_vdw=param_vdw, patch_file=patch_file)
 
-                        # Assign parameters
-                        atFeat.assign_parameters()
+                # Assign parameters
+                atFeat.assign_parameters()
+                
+                try:
+                    # Compute the pair interactions
+                    atFeat.evaluate_pair_interaction()
 
-                        # Compute the pair interactions
-                        atFeat.evaluate_pair_interaction()
+                    # Get the contact atoms
+                    indA, indB = atFeat.sqldb.get_contact_atoms()
 
-                        # Get the contact atoms
-                        indA, indB = atFeat.sqldb.get_contact_atoms()  # extend_to_residue=True ?
+                    # Create "point cloud"
+                    pcA = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge', rowID=indA))
+                    pcB = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge', rowID=indB))
+                    pcA = np.c_[pcA, np.zeros_like(pcA)]
+                    pcB = np.c_[pcB, np.zeros_like(pcB)]
+                    pc = np.r_[pcA, pcB].astype(np.float32)
 
-                        # Create "point cloud"
-                        pc = np.array(atFeat.sqldb.get(
-                            'x,y,z,eps,sig,charge,chainID', rowID=indA+indB))
+                    # Get iRMSD
+                    sim = StructureSimilarity(decoy_dir+'/'+decoy_name, arg.root_dir+'natives/'+native_name)
+                    irmsd = sim.compute_irmsd_fast(method='svd', izone='izone/'+native_name[:4]+'.izone')
 
-                        # Get iRMSD
-                        sim = StructureSimilarity(
-                            decoy_dir+'/'+decoy_name, arg.root_dir+'natives/'+native_name)
-                        irmsd = sim.compute_irmsd_fast(
-                            method='svd', izone=native_name[:4]+'.izone')
+                    # Save file
+                    ds = subgroup.create_dataset(decoy_name[:-4], data = pc)
+                    ds.attrs['irmsd'] = irmsd
+                    print(decoy_name[:-4], 'done')
+                except KeyboardInterrupt:
+                    hf.close()
+                #except:
+                    #print(decoy_name[:-4], 'did not contain contact atoms')
+                break """
+    
 
-                        # Convert encoding
-                        integer_encoded = label_encoder.fit_transform(pc[:, 6])
-                        integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-                        onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-                        pc = np.hstack((pc[:, :6], onehot_encoded))
-                        # Convert to float because, unfortunately, the sql returns everything as strings
-                        pc = np.array(pc).astype(np.float32)
-
-                        # Save file
-                        with open(pc_file, "wb") as f:
-                            pickle.dump([irmsd, pc], f)
-                        print(decoy_name[:-4], 'done')
-                    except KeyboardInterrupt:
-                        sys.exit()
-                    except:
-                        print(decoy_name[:-4], 'did not contain contact atoms')
-
-
-for native_name in sorted(os.listdir(arg.root_dir+'natives/')):
-    convertFolder(native_name)
+hq = h5py.File('pointclouds.h5', 'r')
+train = hq.get('train/1AVX')
+ds=train.get('1AVX_100w')
+print(ds.dtype)
+print(np.array(ds))
