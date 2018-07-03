32a4c8e59e5eb34a4fdd7b91203f944de076ebdb
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-03 13:07:27 +0200

    Adapt to HDF5 & dockQ

--- dataset.py
+++ dataset.py
@@ -4,34 +4,33 @@ import pickle
 import os
 import random
 import numpy as np
+import h5py
 
 
 class PDBset(data.Dataset):
-    def __init__(self, root_dir, num_points, train=True):
-        self.root_dir = root_dir
+    def __init__(self, hdf5_file, num_points, group='train'):
+        self.hf = h5py.File(hdf5_file,'r')
         self.num_points = num_points
-        if train:
-            self.subfolder = 'train/'
-        else:
-            self.subfolder = 'test/'
-
-        self.file_list = os.listdir(root_dir + self.subfolder)
+        self.keys = list(self.hf[group].keys())
 
     def __len__(self):
-        return len(self.file_list)
+        return len(self.keys)
 
     def __getitem__(self, idx):
-        with open(self.root_dir + self.subfolder + self.file_list[idx], "rb") as f:
-            irmsd, pc = pickle.load(f)
-            # Duplicate points within pointcloud don't matter in PointNet architecture due to maxpooling
-            if len(pc) < self.num_points:
-                point_ids = random.sample(range(len(pc)), len(pc))
-                while len(point_ids)<self.num_points-len(pc):
-                    point_ids += random.sample(range(len(pc)), len(pc))
-                point_ids += random.sample(range(len(pc)), self.num_points-len(point_ids))
-            else:
-                point_ids = random.sample(range(len(pc)), self.num_points)
+        ds = self.hf.get(self.keys[idx])
+        pc = np.array(ds)
+        dockQ = ds.attrs['dockQ']
 
-            pc = np.take(pc, point_ids, axis=0)
+        # Duplicate points within pointcloud don't matter in PointNet architecture due to maxpooling
+        # This does NOT apply to avgpooling
+        if len(pc) < self.num_points:
+            point_ids = random.sample(range(len(pc)), len(pc))
+            while len(point_ids)<self.num_points-len(pc):
+                point_ids += random.sample(range(len(pc)), len(pc))
+            point_ids += random.sample(range(len(pc)), self.num_points-len(point_ids))
+        else:
+            point_ids = random.sample(range(len(pc)), self.num_points)
 
-        return torch.from_numpy(pc).float(), np.float32(irmsd)
+        pc = np.take(pc, point_ids, axis=0)
+
+        return torch.from_numpy(pc), np.float32(dockQ)
