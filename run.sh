65025c110d4b6dd11e145e6b24015b816323ca03
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-03 16:58:10 +0200

    Added data retrieval

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python train.py --data_path $1 --out_folder $2  --num_epoch 3 --cosine_decay --batch_size 100 --num_workers 4 --num_points 500| tee log.txt
+wget -P $1 $3
+python train.py --data_path $1 --out_folder $2  --num_epoch 3 --cosine_decay --batch_size 100 --num_workers 4 --num_points 500| tee $2/log.txt
