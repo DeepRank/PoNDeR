71ade6e704050962e3187c367bbb1c3d85dde94f
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-04 10:22:37 +0200

    Revert to single atoms + dual split

--- extract_pc.py
+++ extract_pc.py
@@ -12,9 +12,9 @@ from deeprank.tools import StructureSimilarity
 
 # Parser
 parser = argparse.ArgumentParser()
-parser.add_argument('--root_dir', type=str, help='Absolute path to data')
-
-arg = parser.parse_args(['--root_dir', '/home/lukas/DR_DATA/'])
+parser.add_argument('--root_dir', type=str, default='/home/lukas/DR_DATA/', help='Absolute path to data')
+parser.add_argument('--dual', dest='dual', default=False, action='store_true',help='Store pointclouds of different proteins separately')
+arg = parser.parse_args()
 
 # Force field provided with deeprank
 FF = pkg_resources.resource_filename('deeprank.features', '') + '/forcefield/'
@@ -23,7 +23,12 @@ param_vdw = FF + 'protein-allhdg5-4_new.param'
 patch_file = FF + 'patch.top'
 
 # Prepare HDF5 file & groups
-hf = h5py.File('pointclouds.h5', 'w')
+if arg.dual:
+    filename = 'dualPointclouds.h5'
+else:
+    filename = 'pointclouds.h5'
+
+hf = h5py.File(filename, 'w')
 
 g_test = hf.create_group('test')
 g_train = hf.create_group('train')
@@ -46,7 +51,7 @@ for native_name in sorted(os.listdir(arg.root_dir+'natives/')):
     if os.path.isdir(decoy_dir):
         group = getGroup(native_name)
         print('Putting', native_name[:4], 'in', group.name)
-        for decoy_name in sorted(os.listdir(decoy_dir)):
+        for decoy_name in os.listdir(decoy_dir):
             # Declare the feature calculator instance
             atFeat = AtomicFeature(decoy_dir+'/'+decoy_name, param_charge=param_charge, param_vdw=param_vdw, patch_file=patch_file)
 
@@ -56,29 +61,25 @@ for native_name in sorted(os.listdir(arg.root_dir+'natives/')):
             # Compute the pair interactions
             atFeat.evaluate_pair_interaction()
 
-            # Get contact pairs, append features
+            # Get contact atoms, append features
                 # x, y, z   -> Coordinates
                 # occ       -> Occupancy
                 # temp      -> Temperature factor (uncertainty)
                 # eps       -> 
-                # sig       -> Sigma (?)
+                # sig       -> Sigma
                 # charge    ->
-            pc_pairs = []
-            index = atFeat.sqldb.get_contact_atoms(return_contact_pairs=True, cutoff=6.5)
+            indA, indB = atFeat.sqldb.get_contact_atoms(cutoff=6.5)
 
-            if index: # If not empty
-                for key,val in index.items():
-                    pc1 = atFeat.sqldb.get('x,y,z,eps,sig,charge',rowID=key)[0]
-                    pc2 = atFeat.sqldb.get('x,y,z,eps,sig,charge',rowID=val)
-                    a = np.array(pc1[0:3], dtype=np.float32)
-                    
-                    for p in pc2:
-                        b = np.array(p[0:3], dtype=np.float32)
-                        dist = np.linalg.norm(a-b) # Euclidian distance
-                        pc_pairs.append(pc1+p+[dist])
-                                
-                # List of atom pair parameters to array
-                pc = np.vstack(pc_pairs).astype(np.float32) 
+            if len(indA)==0: # If no contact atoms found
+                print(decoy_name[:-4], 'did not contain contact atoms')
+            else: 
+                pcA = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge', rowID=indA)).astype(np.float32)
+                pcB = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge', rowID=indB)).astype(np.float32)
+
+                if arg.dual:
+                    pcA = np.c_[pcA, np.zeros_like(pcA)]
+                    pcB = np.c_[np.zeros_like(pcB), pcB]
+                    pc = np.r_[pcA, pcB].astype(np.float32)
 
                 # Get metrics
                 sim = StructureSimilarity(decoy_dir+'/'+decoy_name, arg.root_dir+'natives/'+native_name)
@@ -88,12 +89,19 @@ for native_name in sorted(os.listdir(arg.root_dir+'natives/')):
                 dockQ = sim.compute_DockQScore(fnat,lrmsd,irmsd)
 
                 # Save file
-                ds = group.create_dataset(decoy_name[:-4], data = pc)
-                ds.attrs['irmsd'] = irmsd
-                ds.attrs['lrmsd'] = lrmsd
-                ds.attrs['fnat']  = fnat
-                ds.attrs['dockQ'] = dockQ
+                if arg.dual:
+                    subgroup = group.create_group(decoy_name[:-4])
+                    dsA = subgroup.create_dataset('A', data = pcA)
+                    dsB = subgroup.create_dataset('B', data = pcB)
+                    subgroup.attrs['irmsd'] = irmsd
+                    subgroup.attrs['lrmsd'] = lrmsd
+                    subgroup.attrs['fnat']  = fnat
+                    subgroup.attrs['dockQ'] = dockQ
+                else:
+                    ds = group.create_dataset(decoy_name[:-4], data = pc)
+                    ds.attrs['irmsd'] = irmsd
+                    ds.attrs['lrmsd'] = lrmsd
+                    ds.attrs['fnat']  = fnat
+                    ds.attrs['dockQ'] = dockQ
                 print('    ',decoy_name[:-4], 'done')
-            else:
-                print(decoy_name[:-4], 'did not contain contact atoms')
 hf.close()
