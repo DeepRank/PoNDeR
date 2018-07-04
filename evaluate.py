eb7518fb8e56107edab5a57b27c4a83f131b8c5a
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-04 10:57:56 +0200

    Whitespace

--- evaluate.py
+++ evaluate.py
@@ -2,7 +2,6 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-
 def evaluateModel(model, loss_func, testloader):
     model.eval()  # Set to testing mode
     cnt = 0
@@ -11,13 +10,17 @@ def evaluateModel(model, loss_func, testloader):
     predictions = []
     for data in testloader:
         points, target = data
+
         points = Variable(points,volatile=True)
         target = Variable(target,volatile=True)
+
         points = points.transpose(2, 1)
         prediction = model(points).view(-1)
         loss = loss_func(prediction, target)
+
         cnt += target.size(0)
         loss_sum += loss.data[0]
+        
         predictions.append(prediction)
         targets.append(target)
     return loss_sum / cnt, torch.cat(targets), torch.cat(predictions)
