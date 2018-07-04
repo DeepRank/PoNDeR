b3a8a6d894c06f7d20aba083d4e06b22c8584c7b
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-04 10:22:05 +0200

    Dual point file reading

--- dataset.py
+++ dataset.py
@@ -6,14 +6,15 @@ import random
 import numpy as np
 import h5py
 
-# No more than one worker can be used for this type of dataset as HDF5 does not multithread appropriately
+# No more than one worker can be used for these types of dataset as HDF5 does not multithread appropriately
 
 class PDBset(data.Dataset):
-    def __init__(self, hdf5_file, num_points, group='train'):
+    def __init__(self, hdf5_file, num_points, group='train', metric='dockQ'):
         self.hf = h5py.File(hdf5_file,'r')
         self.num_points = num_points
         self.group = self.hf[group]
         self.keys = list(self.group.keys())
+        self.metric = metric
 
     def __len__(self):
         return len(self.keys)
@@ -21,18 +22,45 @@ class PDBset(data.Dataset):
     def __getitem__(self, idx):
         ds = self.group.get(self.keys[idx])
         pc = np.array(ds)
-        dockQ = ds.attrs['dockQ']
+        mtrc = ds.attrs[self.metric]
 
-        # Duplicate points within pointcloud don't matter in PointNet architecture due to maxpooling
-        # This does NOT apply to avgpooling
-        if len(pc) < self.num_points:
-            point_ids = random.sample(range(len(pc)), len(pc))
-            while len(point_ids)<self.num_points-len(pc):
-                point_ids += random.sample(range(len(pc)), len(pc))
-            point_ids += random.sample(range(len(pc)), self.num_points-len(point_ids))
-        else:
-            point_ids = random.sample(range(len(pc)), self.num_points)
+        pc = samplePoints(pc, self.num_points)
 
-        pc = np.take(pc, point_ids, axis=0)
+        return torch.from_numpy(pc), np.float32(mtrc)
 
-        return torch.from_numpy(pc), np.float32(dockQ)
+class DualPDBset(data.Dataset):
+    def __init__(self, hdf5_file, num_points, group='train', metric='dockQ'):
+        self.hf = h5py.File(hdf5_file,'r')
+        self.num_points = num_points
+        self.group = self.hf[group]
+        self.keys = list(self.group.keys())
+        self.metric = metric
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, idx):
+        subgroup = self.group.get(self.keys[idx])
+        pcA = np.array(subgroup.get('A'))
+        pcB = np.array(subgroup.get('B'))
+        mtrc = subgroup.attrs[self.metric]
+
+        pcA = samplePoints(pcA, self.num_points)
+        pcB = samplePoints(pcB, self.num_points)
+
+        return torch.from_numpy(pcA), torch.from_numpy(pcB), np.float32(mtrc)
+
+
+# Duplicate points within pointcloud don't matter in PointNet architecture due to maxpooling
+# This does NOT apply to avgpooling!
+def samplePoints(pc, num_points):
+    if len(pc) < num_points:
+        point_ids = random.sample(range(len(pc)), len(pc))
+        while len(point_ids)<num_points-len(pc):
+            point_ids += random.sample(range(len(pc)), len(pc))
+        point_ids += random.sample(range(len(pc)), num_points-len(point_ids))
+    else:
+        point_ids = random.sample(range(len(pc)), num_points)
+
+    pc = np.take(pc, point_ids, axis=0)
+    return pc
