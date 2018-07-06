598eebedbc02f168a490894a8b7c9414ba712d3a
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-06 14:13:15 +0200

    Elaborate print test

--- dataset.py
+++ dataset.py
@@ -23,7 +23,7 @@ class PDBset(data.Dataset):
         pcA = np.array(subgroup.get('A'))
         pcB = np.array(subgroup.get('B'))
         mtrc = subgroup.attrs[self.metric]
-        print(mtrc)
+        print(self.metric, subgroup.attrs['dockQ'], subgroup.attrs['irmsd'], subgroup.attrs['lrmsd'], subgroup.attrs['fnat'])
 
         pcA = np.c_[pcA, np.zeros_like(pcA)]
         pcB = np.c_[np.zeros_like(pcB), pcB]
