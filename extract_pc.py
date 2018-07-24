f209064412b6f3a4718dcf91a5622cfe191d6836
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-24 10:47:09 +0200

    Reworked entire code, added full cloud extraction

--- extract_pc.py
+++ extract_pc.py
@@ -10,6 +10,15 @@ import random
 from deeprank.features import AtomicFeature
 from deeprank.tools import StructureSimilarity
 
+'''
+x, y, z   -> Coordinates
+occ       -> Occupancy
+temp      -> Temperature factor (uncertainty)
+eps       -> 
+sig       -> Sigma
+charge    ->
+'''
+
 # Parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_dir', type=str, default='/home/lukas/DR_DATA/', help='Path to data')
@@ -24,7 +33,6 @@ parser.add_argument('--minimal', dest='minimal', default=False, action='store_tr
 arg = parser.parse_args()
 
 # Check for incompatibilities
-
 if arg.dual and arg.pairs:
     raise AttributeError('Dual and pair options are incomplatible')
 
@@ -55,6 +63,8 @@ g_holdout = hf.create_group('holdout')
 # Feature width
 if arg.dual:
     hf.attrs['feat_width'] = 8
+elif arg.full_cloud:
+    hf.attrs['feat_width'] = 6
 elif arg.pairs:
     hf.attrs['feat_width'] = 17
 else:
@@ -73,6 +83,72 @@ def getGroup(native_name):
         group = g_holdout
     return group
 
+# Concatenate (dual -> single format)
+def zeroPadConcat(pcA, pcB):
+    pcA = np.c_[pcA, np.zeros_like(pcA)] # Pad right
+    pcB = np.c_[np.zeros_like(pcB), pcB] # Pad left
+    return np.r_[pcA, pcB]
+
+# Extract contact atoms in dual format
+def getDual(atFeat):
+    indA, indB = atFeat.sqldb.get_contact_atoms(cutoff=7) # Get contact atoms
+
+    if len(indA)==0: # If no contact atoms found
+        return None, None
+    else:
+        pcA = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', rowID=indA)).astype(np.float32)
+        pcB = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', rowID=indB)).astype(np.float32)
+    return pcA, pcB
+
+# Extract full point cloud in dual format
+def getFull(atFeat):
+    pcA = np.array(atFeat.sqldb.get('x,y,z,charge,temp,occ', chainID='A')).astype(np.float32)
+    pcB = np.array(atFeat.sqldb.get('x,y,z,charge,temp,occ', chainID='B')).astype(np.float32)
+    return pcA, pcB
+
+# Extract contact atoms in single format
+def getSingle(atFeat):
+    indA, indB = atFeat.sqldb.get_contact_atoms(cutoff=7) # Get contact atoms
+
+    if len(indA)==0: # If no contact atoms found
+        return None
+    else:
+        pcA = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', rowID=indA)).astype(np.float32)
+        pcB = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', rowID=indB)).astype(np.float32)
+        pcA = np.c_[pcA, np.zeros_like(pcA)] # Pad right
+        pcB = np.c_[np.zeros_like(pcB), pcB] # Pad left
+        pc  = np.r_[pcA, pcB]
+    return pc
+
+# Extract contact atom pairs in single format
+def getPairs(atFeat):
+    index = atFeat.sqldb.get_contact_atoms(return_contact_pairs=True, cutoff=7) # Get contact atoms
+    pc_pairs = []
+
+    if index: # If not empty
+        for key,val in index.items():
+            pc1 = atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ',rowID=key)[0]
+            pc2 = atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ',rowID=val)
+            a = np.array(pc1[0:3], dtype=np.float32)
+            
+            for p in pc2:
+                b = np.array(p[0:3], dtype=np.float32)
+                dist = np.linalg.norm(a-b) # Euclidian distance
+                pc_pairs.append(pc1+p+[dist])
+        
+        pc = np.vstack(pc_pairs).astype(np.float32) # List of atom pair parameters to array
+    else: # If no contact atoms found
+        return None
+    return pc
+
+# Calculate scoring metrics
+def getMetrics(sim):
+    irmsd = sim.compute_irmsd_fast(method='svd')
+    lrmsd = sim.compute_lrmsd_fast(method='svd')
+    fnat = sim.compute_Fnat_fast()
+    dockQ = sim.compute_DockQScore(fnat,lrmsd,irmsd)
+    return irmsd, lrmsd, fnat, dockQ
+
 # Start converting
 for native_name in sorted(os.listdir(arg.root_dir+arg.native_dir)):
     decoy_dir = arg.root_dir+arg.decoy_dir+native_name[:4]+'/'+arg.decoy_subdir
@@ -82,68 +158,20 @@ for native_name in sorted(os.listdir(arg.root_dir+arg.native_dir)):
         for decoy_name in sorted(os.listdir(decoy_dir)):
             # Declare the feature calculator instance
             atFeat = AtomicFeature(decoy_dir+'/'+decoy_name, param_charge=param_charge, param_vdw=param_vdw, patch_file=patch_file)
+            atFeat.assign_parameters() # Assign parameters
+            if not arg.full_cloud:
+                atFeat.evaluate_pair_interaction() # Compute the pair interactions
+            sim = StructureSimilarity(decoy_dir+'/'+decoy_name, arg.root_dir+arg.native_dir+native_name)
 
-            # Assign parameters
-            atFeat.assign_parameters()
-            
-            # Compute the pair interactions
-            atFeat.evaluate_pair_interaction()
+            # Dual types
+            if arg.dual or arg.full_cloud: 
+                if arg.dual: 
+                    pcA, pcB = getDual(atFeat)
+                elif arg.full_cloud:
+                    pcA, pcB = getFull(atFeat)
 
-            cont = True
-            if arg.pairs: # Pairwise extraction
-                index = atFeat.sqldb.get_contact_atoms(return_contact_pairs=True, cutoff=7) # Get contact atoms
-                pc_pairs = []
-
-                if index: # If not empty
-                    for key,val in index.items():
-                        pc1 = atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ',rowID=key)[0]
-                        pc2 = atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ',rowID=val)
-                        a = np.array(pc1[0:3], dtype=np.float32)
-                        
-                        for p in pc2:
-                            b = np.array(p[0:3], dtype=np.float32)
-                            dist = np.linalg.norm(a-b) # Euclidian distance
-                            pc_pairs.append(pc1+p+[dist])
-                    
-                    pc = np.vstack(pc_pairs).astype(np.float32) # List of atom pair parameters to array
-                else: # If no contact atoms found
-                    cont = False                
-                
-            else: # Single atom extraction
-                indA, indB = atFeat.sqldb.get_contact_atoms(cutoff=7) # Get contact atoms
-
-                if len(indA)==0: # If no contact atoms found
-                    cont = False
-                else:
-                    '''
-                    x, y, z   -> Coordinates
-                    occ       -> Occupancy
-                    temp      -> Temperature factor (uncertainty)
-                    eps       -> 
-                    sig       -> Sigma
-                    charge    ->
-                    '''
-                    if arg.full_cloud: # All atoms
-                        pcA = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', chainID='A')).astype(np.float32)
-                        pcB = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', chainID='B')).astype(np.float32)
-                    else: # Only contact atoms
-                        pcA = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', rowID=indA)).astype(np.float32)
-                        pcB = np.array(atFeat.sqldb.get('x,y,z,eps,sig,charge,temp,occ', rowID=indB)).astype(np.float32)
-
-                    if not arg.dual: # Pad with zeros and concatenate
-                        pcA = np.c_[pcA, np.zeros_like(pcA)] # Pad right
-                        pcB = np.c_[np.zeros_like(pcB), pcB] # Pad left
-                        pc  = np.r_[pcA, pcB]
-            if cont:
-                # Get metrics
-                sim = StructureSimilarity(decoy_dir+'/'+decoy_name, arg.root_dir+arg.native_dir+native_name)
-                irmsd = sim.compute_irmsd_fast(method='svd')
-                lrmsd = sim.compute_lrmsd_fast(method='svd')
-                fnat = sim.compute_Fnat_fast()
-                dockQ = sim.compute_DockQScore(fnat,lrmsd,irmsd)
-
-                # Save file
-                if arg.dual:
+                if pcA is not None:
+                    irmsd, lrmsd, fnat, dockQ = getMetrics(sim)
                     subgroup = group.create_group(decoy_name[:-4])
                     dsA = subgroup.create_dataset('A', data = pcA)
                     dsB = subgroup.create_dataset('B', data = pcB)
@@ -152,16 +180,29 @@ for native_name in sorted(os.listdir(arg.root_dir+arg.native_dir)):
                     subgroup.attrs['fnat']  = fnat
                     subgroup.attrs['dockQ'] = dockQ
                 else:
+                    print('    ',decoy_name[:-4], 'did not contain contact atoms')
+                    break
+
+            # Non-dual types
+            else: 
+                if arg.pairs: # Pairwise extraction
+                    pc = getPairs(atFeat)
+                else:
+                    pc = getSingle(atFeat)
+                
+                if pc is not None: 
+                    irmsd, lrmsd, fnat, dockQ = getMetrics(sim)
                     ds = group.create_dataset(decoy_name[:-4], data = pc)
                     ds.attrs['irmsd'] = irmsd
                     ds.attrs['lrmsd'] = lrmsd
                     ds.attrs['fnat']  = fnat
                     ds.attrs['dockQ'] = dockQ
-                print('    ',decoy_name[:-4], 'done')
-            else: 
-                print('    ',decoy_name[:-4], 'did not contain contact atoms')
+                else:
+                    print('    ',decoy_name[:-4], 'did not contain contact atoms')
+                    break
 
-            if arg.minimal: # For testing this script
+            print('    ',decoy_name[:-4], 'done')
+            if arg.minimal: # For testing this script, only one per loop
                 break
     else:
         print(decoy_dir, 'not found')
