eaa1378c4a4c637358ed34a292560dfd7156f35c
Lukas De Clercq <declercqlukas+github@gmail.com>
2018-07-24 12:01:00 +0200

    Remove data offloading (done in train.py)

--- plotLoss.py
+++ plotLoss.py
@@ -7,8 +7,8 @@ import matplotlib.axes as axs
 
 def scatter(x1, y1, x2, y2, test_score, save_path):
     fig, ax = plt.subplots()
-    ax.scatter(x2.data.cpu(),y2.data.cpu(), label='Train',s=1)
-    ax.scatter(x1.data.cpu(),y1.data.cpu(), label='Test' ,s=1)
+    ax.scatter(x2,y2, label='Train',s=1)
+    ax.scatter(x1,y1, label='Test' ,s=1)
     ax.set_ylabel('Prediction')
     ax.set_xlabel('Truth')
     ax.set_xlim(xmin=0.0) # All scores are > 0
